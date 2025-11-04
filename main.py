df9d45f3bcb3aca492090bbffdd40ecc3afd71fb
cyuber.devoloper <sabdumajid91@gmail.com>
2025-11-04 21:57:59 +0500

    coment

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from time import sleep
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 load_dotenv()
 LOGIN_URL = os.getenv("LOGIN_URL")
@@ -35,6 +35,8 @@ try:
         pwd_el.send_keys(os.getenv("LOGIN_PASSWORD"))
     submit_el = wait.until(EC.element_to_be_clickable(submit_locator))
     submit_el.click()
+    button = driver.find_element(By.CLASS_NAME, "btn-primary")
+    button.click()
     sleep(1000)
 finally:
     driver.quit()
--- python ocodemy.py
+++ python ocodemy.py
@@ -1,22 +1,41 @@
-import mysql.connector
+# import mysql.connector
+#
+# # 🔹 Ma’lumotlar bazasiga ulanish
+# db = mysql.connector.connect(
+#     host="localhost",     # server manzili
+#     user="linux",          # foydalanuvchi
+#     password="linux128",     # parol
+#     database="school_db "  # bazaning nomi
+# )
+#
+# cursor = db.cursor()
+#
+# # 🔹 So‘rov yuborish
+# cursor.execute("SELECT COUNT(*) AS count FROM students WHERE grade = '10 B';")
+#
+# # 🔹 Natijani olish
+# result = cursor.fetchone()
+# print("10-B sinfidagi o‘quvchilar soni:", result[0])
+#
+# # 🔹 Yopish
+# cursor.close()
+# db.close()
+class Person:
+    def __init__(self,ism,yosh):
+        self.name = ism
+        self.age = yosh
 
-# 🔹 Ma’lumotlar bazasiga ulanish
-db = mysql.connector.connect(
-    host="localhost",     # server manzili
-    user="linux",          # foydalanuvchi
-    password="linux128",     # parol
-    database="school_db "  # bazaning nomi
-)
+    def commare_age(self,other):
+        if self.age < other.yosh:
+            return f"{other.ism} mendan kata"
+        elif self.age > other.yosh:
+            return f"{other.ism} mendan kichik"
+        else:
+            return f"men { other.ism} bilanan teng"
+p1 = Person("sayod",15)
+p2 = Person("ziyo",15)
+p3 = Person("isim", 14)
 
-cursor = db.cursor()
-
-# 🔹 So‘rov yuborish
-cursor.execute("SELECT COUNT(*) AS count FROM students WHERE grade = '10 B';")
-
-# 🔹 Natijani olish
-result = cursor.fetchone()
-print("10-B sinfidagi o‘quvchilar soni:", result[0])
-
-# 🔹 Yopish
-cursor.close()
-db.close()
+print(p1.commare_age(p1))
+print(p2.commare_age(p2))
+print(p3.commare_age(p3))
